9ecd3e4cc1b8235ea416615d2fdcee9e8def337e
Kohei Asai <yo@kohei.dev>
2023-06-02 10:35:21 -0700

    chore(release): set up semantic release

---
+++ .releaserc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: [{ name: "main" }],
+  plugins: [
+    ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/npm", { npmPublish: false }],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message: "chore(release): update `package.json` version to ${nextRelease.version}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
