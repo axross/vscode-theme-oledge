634f7989a54f0fc8e5ab4c0b309594899c7db7d5
Kohei Asai <yo@kohei.dev>
2023-06-02 11:31:50 -0700

    chore(release): add semantic-release changelog plugin

--- .releaserc.js
+++ .releaserc.js
@@ -3,12 +3,14 @@ module.exports = {
   plugins: [
     ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
     "@semantic-release/release-notes-generator",
+    ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
     ["@semantic-release/npm", { npmPublish: false }],
     [
       "@semantic-release/git",
       {
         assets: ["CHANGELOG.md", "package.json"],
-        message: "chore(release): update `package.json` version to ${nextRelease.version}",
+        message:
+          "chore(release): update `package.json` version to ${nextRelease.version}",
       },
     ],
     "@semantic-release/github",
