0c8e5f82cc1ec8c62d74ca7eb6331478cb4fb0e4
Kohei Asai <yo@kohei.dev>
2023-06-02 16:49:56 -0700

    chore(release): update gh action

--- release.config.js
+++ release.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
     ["@semantic-release/npm", { npmPublish: false }],
+    "semantic-release-vsce",
     [
       "@semantic-release/git",
       {
